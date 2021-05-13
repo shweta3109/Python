98620c26b2ef3ed64528accd810f798a286ae197
shweta3109 <51168393+shweta3109@users.noreply.github.com>
2021-05-13 19:52:44 -0400

    for & while

---
+++ q20.py
@@ -0,0 +1,8 @@
+BirdsName=["Parrot","Pigeon","Eagle"]
+[print(x) for x in BirdsName]
+for i in range(len(BirdsName)):
+   print(BirdsName[i])
+i=0
+while i<len(BirdsName):
+    print(BirdsName[i])
+    i=i+1
