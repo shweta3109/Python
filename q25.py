6cfadafa34b8ce3790b17100a6290124641d3762
shweta3109 <51168393+shweta3109@users.noreply.github.com>
2021-05-15 23:21:05 -0400

    access items from set(loop)

---
+++ q25.py
@@ -0,0 +1,12 @@
+myDict={
+    "class": 5,
+    "section": "rama",
+    "subject":"science"
+
+}
+for x in myDict.values():
+     print(x)
+for y in myDict.items():
+    print(y)  
+for z in myDict.keys():
+    print(z)   
