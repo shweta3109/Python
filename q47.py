f0827ef416574dd04c444de8f76d4d6428618fbf
shweta3109 <51168393+shweta3109@users.noreply.github.com>
2021-05-26 20:55:15 -0400

    plotting1+

---
+++ q47.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+y1=np.array([0,2,7,9])
+y2=np.array([3,5,7,8])
+
+plt.plot(y1,lw='10',color='red')
+plt.plot(y2,lw='15',color='green')
+plt.show()
+exit()
