79b1ecb23e0be1b60c157e0fe34e264d29fb6beb
shweta3109 <51168393+shweta3109@users.noreply.github.com>
2021-06-04 18:30:18 -0400

    Arithmetic Operations on Arrays

---
+++ q73.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr1=np.array([10,7,8])
+arr2=np.array([2,2,3])
+arr3=np.divmod(arr1,arr2)
+arr4=np.absolute(arr1)
+arr5=np.power(arr1,arr2)
+print(arr3)
+print(arr4)
+print(arr5)
